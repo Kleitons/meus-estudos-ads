c5fd029805c150a582ed6292f39c8e78039dd684
Kleiton Simões <kleitonsimoes@gmail.com>
2026-09-01 15:40:49 -0300

    Atividades atualizadas

---
+++ FaculdadeDescomplica/desenvolvimentoDinamico/modulo4-instrucoesRepeticoes/weatherDecision.js
@@ -0,0 +1,11 @@
+// Variável que representa a previsão do tempo
+// Pode ser "rainy" (Chuvoso), "sunny" (Ensolarado) ou "cloudy" (Nublado)
+
+let weatherForecast = "sunny";
+
+// Analogia com a vida real: Decidindo se devemos levar um guarda-chuva
+if(weatherForecast === "rainy") {
+    console.log("Devemos levar um guarda-chuva.");
+} else {
+    console.log("Não devemos levar um guarda-chuva.");
+}
